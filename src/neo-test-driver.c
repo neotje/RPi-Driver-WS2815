448a69aca5ef0883c28755f373942b457aa27936
Neo Hop <neotje111@gmail.com>
2022-05-20 17:12:26 +0100

    proc test

--- src/neo-test-driver.c
+++ src/neo-test-driver.c
@@ -2,9 +2,46 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+#include <linux/proc_fs.h>
+#include <linux/slab.h>
+
+#include <asm/io.h>
+
+#define TEST_DRIVER_MAX_SIZE 1024
+
+static struct proc_dir_entry *test_proc = NULL;
+
+static char data_buffer[TEST_DRIVER_MAX_SIZE];
+
+ssize_t test_proc_read(struct file *file, char __user *user, size_t size, loff_t *offset)
+{
+    return copy_to_user(user, "test_proc_read\n", 15);
+}
+
+ssize_t test_proc_write(struct file *file, const char __user *user, size_t size, loff_t *offset)
+{
+    memset(data_buffer, 0x0, sizeof(data_buffer));
+
+    if (size > TEST_DRIVER_MAX_SIZE)
+        size = TEST_DRIVER_MAX_SIZE;
+
+    copy_from_user(data_buffer, user, size);
+
+    printk(KERN_INFO "test_proc_write: '%s'\n", data_buffer);
+    return size;
+}
+
+static const struct proc_ops test_proc_ops = {
+    .proc_read = test_proc_read, // this function gets executed when the user reads the proc file (e.g. cat /proc/neo-proc).
+    .proc_write = test_proc_write, // this function gets executed when the user writes to the proc file (e.g. echo "test" > /proc/neo-proc).
+};
+
 static int __init test_driver_init(void)
 {
     printk("Welcome to neo's driver!\n");
+
+    test_proc = proc_create("neo-proc", 0666, NULL, &test_proc_ops);
+
     return 0;
 }
 
